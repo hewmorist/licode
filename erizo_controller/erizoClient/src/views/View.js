054ce3deb6a010ae3477d79d8b7a9945e50eb80f
hewmorist <hewmorist@yahoo.com>
2014-11-15 17:16:32 -0800

    changed asset url in erizo client view

--- erizo_controller/erizoClient/src/views/View.js
+++ erizo_controller/erizoClient/src/views/View.js
@@ -11,6 +11,6 @@ Erizo.View = function (spec) {
     // Variables
 
     // URL where it will look for icons and assets
-    that.url = "http://chotis2.dit.upm.es:3000";
+    that.url = "http://10.0.0.8:8000";
     return that;
 };
