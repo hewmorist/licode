57f1ae125372b9cd32078d876852c837f3f2524b
root <root@peter-TP45E-Combo.(none)>
2015-12-10 23:05:35 -0800

    added ssl version of basicExample

--- extras/basic_example-orig/public/script.js
+++ extras/basic_example-orig/public/script.js
@@ -65,18 +65,13 @@ window.onload = function () {
           var stream = streams[index];
           if (localStream.getID() !== stream.getID()) {
             room.subscribe(stream);
-            stream.addEventListener("bandwidth-alert", function (evt){
-                console.log("Bandwidth Alert", evt.msg, evt.bandwidth);
-            });
-            
-
           }
         }
       };
 
       room.addEventListener("room-connected", function (roomEvent) {
 
-        room.publish(localStream, {maxVideoBW: 3000, minVideoBW:500});
+        room.publish(localStream, {maxVideoBW: 300});
         subscribeToStreams(roomEvent.streams);
       });
 
